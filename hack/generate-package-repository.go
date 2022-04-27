4e40ef2f6adb83c40986e762049e14f0b95bdca7
Rajas Kakodkar <rkakodkar@vmware.com>
2022-04-27 20:05:35 +0530

    add repo creation automation and deck package
    Signed-off-by: Rajas Kakodkar <rkakodkar@vmware.com>

---
+++ hack/generate-package-repository.go
@@ -0,0 +1,149 @@
+// Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package main is a utility program used to generate TCE addon manifests
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Package struct {
+	Name     string   `yaml:"name"`
+	Versions []string `yaml:"versions"`
+}
+
+type Repository struct {
+	Packages []Package `yaml:"packages"`
+}
+
+type BundleLock struct {
+	APIVersion string    `json:"apiVersion"`
+	Kind       string    `json:"kind"`
+	Bundle     BundleRef `json:"bundle"` // This generated yaml, but due to lib we need to use `json`
+}
+
+type BundleRef struct {
+	Image string `json:"image,omitempty"` // This generated yaml, but due to lib we need to use `json`
+	Tag   string `json:"tag,omitempty"`   // This generated yaml, but due to lib we need to use `json`
+}
+
+func main() {
+	var OciRegistry = "public.ecr.aws/t0q8k6g2/repo"
+	var PackagesDirectoryPath = filepath.Join("..", "packages")
+	var RepoDirectoryPath = filepath.Join("..", "repos")
+	var GeneratedRepoDirectoryPath = filepath.Join(RepoDirectoryPath, "generated")
+	var repository Repository
+
+	channel := os.Args[1]
+	tag := os.Args[2]
+	channelDir := filepath.Join(GeneratedRepoDirectoryPath, channel)
+	imgpkgDir := filepath.Join(channelDir, ".imgpkg")
+	packagesDir := filepath.Join(channelDir, "packages")
+
+	// Remove any existing generated files
+	os.RemoveAll(channelDir)
+	err := os.MkdirAll(imgpkgDir, 0755)
+	check(err)
+
+	err = os.MkdirAll(packagesDir, 0755)
+	check(err)
+
+	targetChannelFilename := filepath.Join(RepoDirectoryPath, channel+".yaml")
+	source, err := os.ReadFile(targetChannelFilename)
+	check(err)
+
+	err = yaml.Unmarshal(source, &repository)
+	check(err)
+
+	var outputPackageYaml = filepath.Join(packagesDir, "packages.yaml")
+	outputFile, err := os.Create(outputPackageYaml)
+	check(err)
+
+	defer func() {
+		if err := outputFile.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for _, p := range repository.Packages {
+		metadataFilepath := getYamlFilepath(filepath.Join(PackagesDirectoryPath, p.Name, "metadata"))
+		copyYaml(metadataFilepath, outputFile)
+
+		for _, version := range p.Versions {
+			packageFilepath := getYamlFilepath(filepath.Join(PackagesDirectoryPath, p.Name, version, "package"))
+			copyYaml(packageFilepath, outputFile)
+		}
+	}
+
+	imagesLockFile := filepath.Join(imgpkgDir, "images.yml")
+	execCommand("kbld", []string{"--file", packagesDir, "--imgpkg-lock-output", imagesLockFile})
+
+	bundleLockFilename := "output.yaml"
+	registryPathAndTag := OciRegistry + "/" + channel + ":" + tag
+	execCommand("imgpkg", []string{"push", "--tty", "--bundle", registryPathAndTag, "--file", channelDir, "--lock-output", bundleLockFilename})
+
+	bundleLockYamlFile, err := os.ReadFile(bundleLockFilename)
+	check(err)
+
+	var bundleLock BundleLock
+	err = yaml.Unmarshal(bundleLockYamlFile, &bundleLock)
+	check(err)
+
+	fmt.Println("Package Repository pushed to", bundleLock.Bundle.Image)
+	fmt.Println("\nInstall with:\ntanzu package repository add repo-name --namespace default --url", bundleLock.Bundle.Image)
+	os.RemoveAll(bundleLockFilename)
+}
+
+func execCommand(command string, commandArgs []string) {
+	output, err := exec.Command(command, commandArgs...).CombinedOutput()
+	if err != nil {
+		log.Fatal(string(output))
+	}
+}
+
+func copyYaml(packageFilepath string, outputFile *os.File) {
+	source, err := os.ReadFile(packageFilepath)
+	check(err)
+
+	var slice = source[0:3]
+	if !strings.HasPrefix(string(slice), "---") {
+		if _, err := outputFile.WriteString("---\n"); err != nil {
+			panic(err)
+		}
+	}
+
+	_, err = outputFile.Write(source)
+	check(err)
+
+	slice = source[len(source)-1:]
+	if string(slice) != "\n" {
+		if _, err := outputFile.WriteString("\n"); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getYamlFilepath(filename string) string {
+	yamlFilepath := filename + ".yaml"
+	_, err := os.Stat(yamlFilepath)
+	if err != nil {
+		yamlFilepath = filename + ".yml"
+		_, err = os.Stat(yamlFilepath)
+		check(err)
+	}
+	return yamlFilepath
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
---
+++ packages/deck/0.1.0/README.md
@@ -0,0 +1,25 @@
+# deck Package
+
+This package provides << awesome functionality >> using [deck](https://INFO_NEEDED).
+
+## Components
+
+## Configuration
+
+The following configuration values can be set to customize the deck installation.
+
+### Global
+
+| Value | Required/Optional | Description |
+|-------|-------------------|-------------|
+| `name` | Optional | description |
+
+### deck Configuration
+
+| Value | Required/Optional | Description |
+|-------|-------------------|-------------|
+| `name` | Optional | description |
+
+## Usage Example
+
+The follow is a basic guide for getting started with deck.
