536dd83184fca15865e13e2754512cc763ee1abe
Amim Knabben <aknabben@vmware.com>
2022-04-27 17:53:36 -0300

    Adding clientset

--- cmd/cli/plugin/prow/cmd/plugin/prow/install.go
+++ cmd/cli/plugin/prow/cmd/plugin/prow/install.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -16,6 +19,36 @@ var InstallCmd = &cobra.Command{
 }
 
 func installProw(cmd *cobra.Command, _ []string) error {
+	var (
+		kubeConfig = getDefaultKubeconfigPath()
+	)
+
+	clientset := getClientSet(kubeConfig)
+	fmt.Println(clientset)
+
 	fmt.Println("One day, some day soon? Install Prow, the repo, all the repo package bundles, and its prerequisites on a workload cluster.")
 	return nil
+}
+
+func getClientSet(kubeConfig string) *kubernetes.Clientset {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	return clientset
+}
+
+func getDefaultKubeconfigPath() string {
+	kubeConfigFilename := os.Getenv(clientcmd.RecommendedConfigPathEnvVar)
+	// fallback to default kubeconfig file location if no env variable set
+	if kubeConfigFilename == "" {
+		kubeConfigFilename = clientcmd.RecommendedHomeFile
+	}
+	return kubeConfigFilename
 }
